24f6b4e14e76b60beaa49079378c88de31c60b69
Morten Linnet <mortenlinnet@hotmail.com>
2018-10-12 09:48:09 +0200

    okojo

--- app/src/main/java/com/example/morte/bennyapp/BennyMain.java
+++ app/src/main/java/com/example/morte/bennyapp/BennyMain.java
@@ -2,6 +2,7 @@ package com.example.morte.bennyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class BennyMain extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class BennyMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_benny_main);
+
+        Toast.makeText(this, "olo", Toast.LENGTH_SHORT).show();
     }
 }
