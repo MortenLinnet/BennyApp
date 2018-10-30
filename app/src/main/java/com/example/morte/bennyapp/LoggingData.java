482fd06949eaf51ebc0fe6ce1566fa6941ab407e
Nilan Paramarajah <nilan974@hotmail.com>
2018-10-30 10:39:54 +0100

    Gravity center i size of eyes + tekstboks på front UI

--- app/src/main/java/com/example/morte/bennyapp/LoggingData.java
+++ app/src/main/java/com/example/morte/bennyapp/LoggingData.java
@@ -1,8 +1,10 @@
 package com.example.morte.bennyapp;
 
+import android.content.Context;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -45,52 +47,7 @@ public class LoggingData extends AppCompatActivity {
         feedback2textview = (TextView) findViewById(R.id.feedback2textview);
     }
 
-    public static void export () {
-        File sd = Environment.getExternalStorageDirectory();
-        String csvFile = "BennyStats.xls";
 
-        File directory = new File(sd.getAbsolutePath());
-
-        //create directory if not exist
-        if (!directory.isDirectory()) {
-            directory.mkdirs();
-        }
-        try {
-            //file path
-            File file = new File(directory, csvFile);
-            WorkbookSettings wbSettings = new WorkbookSettings();
-            wbSettings.setLocale(new Locale(Locale.ENGLISH.getLanguage(), Locale.ENGLISH.getCountry()));
-
-            WritableWorkbook workbook;
-            workbook = Workbook.createWorkbook(file, wbSettings);
-
-            //Excel sheetA first sheetA
-            WritableSheet sheetA = workbook.createSheet("sheet A", 0);
-
-            // column and row titles
-            sheetA.addCell(new Label(0, 0, "sheet A 1"));
-            sheetA.addCell(new Label(1, 0, "Sheet A 2"));
-            sheetA.addCell(new Label(0, 1, "Sheet A 3"));
-            sheetA.addCell(new Label(1, 1, "Sheet A 4"));
-
-
-            // close workbook
-            workbook.write();
-            workbook.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void click (View view) {
-
-        // Husk disse lines et sted hvor det giver mening
-        // permissionForExcelExport(Manifest.permission.READ_EXTERNAL_STORAGE, READ_EXST);
-        // permissionForExcelExport(Manifest.permission.WRITE_EXTERNAL_STORAGE, WRITE_EXST);
-        // export();
-
-    }
 
 
 }
