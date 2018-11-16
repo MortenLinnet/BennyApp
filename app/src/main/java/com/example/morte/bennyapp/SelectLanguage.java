bdcf81c875fc29c6a70f5d13102a86c881484120
Nilan <nilan974@hotmail.com>
2018-11-16 18:27:44 +0100

    Backup

---
+++ app/src/main/java/com/example/morte/bennyapp/SelectLanguage.java
@@ -0,0 +1,13 @@
+package com.example.morte.bennyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectLanguage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_language);
+    }
+}
